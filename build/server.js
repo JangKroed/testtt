24f94bbc6d9a78f253cef74ae5de273a01177319
JangKroed <didlsdydgh@gmail.com>
2022-11-14 23:40:01 +0900

    test

--- build/server.js
+++ build/server.js
@@ -5,7 +5,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./src/app"));
 const config_env_1 = __importDefault(require("./src/config.env"));
-app_1.default.listen(config_env_1.default.PORT, () => {
-    console.log(config_env_1.default.NODE_ENV);
-    console.log('SERVER RUNNING ON ', config_env_1.default.PORT);
-});
+const socket_io_1 = require("socket.io");
+const cluster_1 = __importDefault(require("cluster"));
+const redis_1 = require("redis");
+const redis_adapter_1 = require("@socket.io/redis-adapter");
+const os_1 = __importDefault(require("os"));
+const numCPUs = os_1.default.cpus().length;
+const sticky_1 = require("@socket.io/sticky");
+if (cluster_1.default.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    (0, sticky_1.setupMaster)(app_1.default, {
+        loadBalancingMethod: 'least-connection', // either "random", "round-robin" or "least-connection"
+    });
+    app_1.default.listen(config_env_1.default.PORT, () => {
+        console.log(config_env_1.default.NODE_ENV);
+        console.log('SERVER RUNNING ON ', config_env_1.default.PORT);
+    });
+    for (let i = 0; i < numCPUs; i++) {
+        cluster_1.default.fork();
+    }
+    cluster_1.default.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster_1.default.fork();
+    });
+}
+else {
+    console.log(`Worker ${process.pid} started`);
+    const pubClient = (0, redis_1.createClient)({
+        url: `redis://${config_env_1.default.REDIS_USER}:${config_env_1.default.REDIS_PASSWORD}@${config_env_1.default.REDIS_HOST}/0`,
+    });
+    const subClient = pubClient.duplicate();
+    const io = new socket_io_1.Server(app_1.default);
+    Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+        io.adapter((0, redis_adapter_1.createAdapter)(pubClient, subClient));
+        (0, sticky_1.setupWorker)(io);
+        io.listen(3000);
+    });
+    io.on('connection', (socket) => {
+        /* ... */
+    });
+}
--- server.ts
+++ server.ts
@@ -1,7 +1,49 @@
 import httpServer from './src/app';
 import env from './src/config.env';
+import { Server } from 'socket.io';
 
-httpServer.listen(env.PORT, () => {
-    console.log(env.NODE_ENV);
-    console.log('SERVER RUNNING ON ', env.PORT);
-});
+import cluster from 'cluster';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+import numCPU from 'os';
+const numCPUs = numCPU.cpus().length;
+import { setupMaster, setupWorker } from '@socket.io/sticky';
+
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    setupMaster(httpServer, {
+        loadBalancingMethod: 'least-connection', // either "random", "round-robin" or "least-connection"
+    });
+    httpServer.listen(env.PORT, () => {
+        console.log(env.NODE_ENV);
+        console.log('SERVER RUNNING ON ', env.PORT);
+    });
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+} else {
+    console.log(`Worker ${process.pid} started`);
+
+    const pubClient = createClient({
+        url: `redis://${env.REDIS_USER}:${env.REDIS_PASSWORD}@${env.REDIS_HOST}/0`,
+    });
+    const subClient = pubClient.duplicate();
+
+    const io = new Server(httpServer);
+    Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+        io.adapter(createAdapter(pubClient, subClient));
+        setupWorker(io);
+        io.listen(3000);
+    });
+
+    io.on('connection', (socket) => {
+        /* ... */
+    });
+}
